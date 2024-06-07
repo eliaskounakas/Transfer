a85a304bbc208e8a11609491827c50d7b17a82d3
elias <elias.kounakas@proton.me>
2024-06-07 21:51:14 +0200

    add tailwind styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
-    mode: 'standalone',
+    mode: 'standalone'
   }),
   redirects: {
     '/': '/dashboard'
-  }
+  },
+  integrations: [tailwind()]
 });
