3b49bf42cce9bec719772ff21e98603aa920a4b7
Elias Kounakas <eliaskounakas@proton.me>
2024-06-13 17:12:08 +0200

    Allow user to upload files on their dashboard

--- src/pages/api/auth/upload.ts
+++ src/pages/api/auth/upload.ts
@@ -1,11 +1,37 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
+import { v4 as uuidv4 } from "uuid";
 
-export const POST: APIRoute = async ({ request, redirect }) => {
+export const POST: APIRoute = async ({ request, cookies, redirect }) => {
+  let userId:string = "";
+  
+  try {
+    const { data: { user } } = await supabase.auth.getUser();
+    if (user !== null) {
+      userId = user.id;
+    }
+    else {
+      userId = "";
+    }
+  }
+  catch (error) {}
+
+
   const formData = await request.formData();
   const file = formData.get("file") as File;
 
   
+  const { data, error } = await supabase
+  .storage
+  .from('files')
+  .upload(userId + "/" + uuidv4(), file, {
+    cacheControl: '3600',
+    upsert: false
+  })
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
 
   return redirect("/dashboard");
 };
