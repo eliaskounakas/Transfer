49206df6e5ce0af0b84160d48da587496429cfcf
Elias Kounakas <eliaskounakas@proton.me>
2024-06-13 16:07:08 +0200

    Configure file upload frontend

---
+++ src/pages/api/auth/upload.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const file = formData.get("file") as File;
+
+  
+
+  return redirect("/dashboard");
+};
